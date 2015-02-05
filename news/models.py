85637e69950de80122e72eb8dbdde56a8d8fb742
Fan-Yu Kuan <fykuan@gmail.com>
2015-02-05 10:43:32 +0800

    修改資料庫欄位結構

--- news/models.py
+++ news/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 class news(models.Model):
    topic = models.CharField(max_length=256)
-   url = models.CharField(max_length=256)
+   url = models.CharField(max_length=512)
    publisher = models.ForeignKey('publisher.publisher')
    description = models.TextField()
    pub_time = models.DateTimeField()
-   f_cutted = models.IntegerField(default=1)
+   f_cutted = models.PositiveSmallIntegerField(default=0)
