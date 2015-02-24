664fc49eb2626a6219259a71afe0a886306f5d7d
Fan-Yu Kuan <fykuan@gmail.com>
2015-02-24 11:59:23 +0800

    效能因素，改成保留八天的資料就好

--- backend/tools/purge_news.py
+++ backend/tools/purge_news.py
@@ -5,21 +5,21 @@ import sys
 
 
 def main(argumenst):
-    conn = MySQLdb.connect(host="127.0.0.1", user="news_fetcher", passwd="news_fetcher", db="news_fetcher")
+    conn = MySQLdb.connect(host="127.0.0.1", user="keywordcloud", passwd="keywordcloud", db="keywordcloud")
     conn.set_character_set('UTF8')
     c = conn.cursor()
     c.execute('SET NAMES UTF8;')
     c.execute('SET CHARACTER SET utf8;')
     c.execute('SET character_set_connection=utf8;')
 
-    sql = "SELECT * FROM news WHERE pub_time < NOW() - INTERVAL 30 DAY ORDER BY pub_time;"
+    sql = "SELECT * FROM news_news WHERE pub_time < NOW() - INTERVAL 8 DAY ORDER BY pub_time;"
     c.execute(sql)
     for row in c.fetchall():
         print "deleting news topic 「%s」(%s)" % (row[1], row[5])
 
     conn.commit()
 
-    sql = "DELETE FROM news WHERE pub_time < NOW() - INTERVAL 30 DAY;"
+    sql = "DELETE FROM news_news WHERE pub_time < NOW() - INTERVAL 8 DAY;"
     c.execute(sql)
     conn.commit()
     conn.close()
--- words/models.py
+++ words/models.py
@@ -4,7 +4,7 @@ class words(models.Model):
     word = models.CharField(max_length=256)
     wordtype = models.CharField(max_length=10)
     parse_time = models.DateTimeField()
-    news_id = models.ForeignKey('news.news')
+    news_id = models.ForeignKey('news.news', on_delete=models.CASCADE)
 
     def relatedNew(self, obj):
         return '%s' % (obj.news_id_id.topic)
